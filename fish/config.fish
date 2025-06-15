639dcce8092e506f0cc11ac49fa5214309a15aed
imshinyu <defnotkhalyl@gmail.com>
2025-06-15 22:06:21 +0100

    part 2

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+#fish_config theme choose "Matugen"
+
+# Created by `pipx` on 2025-06-13 18:28:33
+set PATH $PATH /home/khalyl/.local/bin
