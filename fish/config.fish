61c166aa6d129d28c546aaac643604f8f316fcbd
imshinyu <defnotkhalyl@gmail.com>
2025-09-05 10:56:58 +0100

    a lot of shit

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,7 @@ end
 # Created by `pipx` on 2025-06-13 18:28:33
 set PATH $PATH /home/khalyl/.local/bin
 alias vim="nvim"
+alias sudec="sudo decman"
+alias videc="vim ~/.config/decman/decman.py"
+set -g theme_color_scheme Matugen
+
